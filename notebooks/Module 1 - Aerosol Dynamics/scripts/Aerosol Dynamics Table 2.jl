ed769d4d64f1d4316994c490bc4d09f5cd649def
Markus Petters <mdpetter@ncsu.edu>
2019-11-26 08:21:38 -0500

    Add aerosol apps [ci skip]

---
+++ notebooks/Module 1 - Aerosol Dynamics/scripts/Aerosol Dynamics Table 2.jl
@@ -0,0 +1,13 @@
+using DifferentialMobilityAnalyzers, DataFrames
+
+𝕟 = lognormal([[200, 80, 1.2]]; d1 = 30.0, d2 = 300.0, bins = 10)
+
+Dp = (𝕟.De[2:end]+𝕟.De[1:end-1])/2.0
+Dlow = round.(Int,𝕟.De[1:end-1])
+Dup = round.(Int,𝕟.De[2:end])
+N = round.(Int,𝕟.N)
+S=π.*(Dp.*1e-3).^2.0.*N
+V=π./6.0*(Dp.*1e-3).^3.0.*N
+M=π./6.0*(Dp.*1e-3).^3.0.*N.*2.0
+
+df = DataFrame(Dlow=Dlow,Dup=Dup,N=N,SpectralDensity=[nothing for i = 1:10])
