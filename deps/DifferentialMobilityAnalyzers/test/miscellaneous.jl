af894b87fabf41e9df14ce460f9375819f96eeec
Markus Petters <mdpetter@ncsu.edu>
2019-12-28 10:41:58 -0500

    Experimental sysimg [ci skip]

--- deps/DifferentialMobilityAnalyzers/test/miscellaneous.jl
+++
@@ -1,8 +0,0 @@
-x = figure("Helvetica", 2, 5.5, 2, 8)
-@test x == 0
-
-pwd()
-cd("../docs/")
-𝕟, rawdp, rawc, Nt_TSI = loadtsidata();
-@test round.(Int,Nt_TSI[[1,10]]) == [197,4979]
-cd("../test/")
--- deps/Fezzik/src/Fezzik.jl
+++
@@ -1,7 +0,0 @@
-module Fezzik
-
-include("blacklist.jl")
-include("bootstrap.jl")
-include("startup.jl")
-
-end # module
